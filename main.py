dc6fad93ca9c19084c1f5fad7fbae2af4a1f3e60
Lena Gavrilov <54853899+lenagavrilov@users.noreply.github.com>
2019-10-05 15:15:15 +0300

    1. moved the data file to the project folder 2. skipped the empty lines in datafile 3. adapted to use for any amount/names of the flowers 4. used mean function

--- main.py
+++ main.py
@@ -1,72 +1,51 @@
-file_name = "C:\\Users\\Lena_Laptop\\Documents\\Python\\FILES\\iris.data"
+import statistics
 
 
-def general_dictionary(filename):
+def general_dictionary():
     global general_list
     general_list = {}
-    with open(filename) as data:
+    with open('iris.data') as data:    # moved file to the project directory
         sample = 1
         for line in data:
             line = line[:len(line) - 1]
+            if line == "":        # added: skip empty line
+                continue
             general_list[sample] = line.split(",")
             sample += 1
-        del general_list[len(general_list)]
-        # print (general_list)
+        # print(general_list)
     return general_list
 
 
-def kind_of_flowers():
-    general_dictionary(file_name)
-    iris_setosa_list()
-    iris_versicolor_list()
-    iris_virginica_list()
+def flowers_quantity(sample):
+    global sample_quantity
+    sample_quantity = {}
+    for element in sample:
+        flower_name = sample[element][4]
+        if flower_name not in sample_quantity.keys():
+            sample_quantity[flower_name] = 1
+        else:
+            sample_quantity[flower_name] += 1
+    # print(sample_quantity)
+    kind_of_flowers()
+    return sample_quantity
 
 
-def iris_setosa_list():
-    iris_setosa = {}
-    count = 1
-    for key in general_list:
-        if 'Iris-setosa' in general_list[key]:
-            iris_setosa[count] = general_list[key]
-            count += 1
-    # print(iris_setosa)
-    count_average(iris_setosa)
-    return iris_setosa
+def kind_of_flowers():         # defining dictionary for each kind of flower
+    for element in sample_quantity:
+        flower_kind = {}
+        count = 1
+        for key in general_list:
+            if element in general_list[key]:
+                flower_kind[count] = general_list[key]
+                count += 1
+        # print(flower_kind)
+        count_average(flower_kind)
 
 
-def iris_versicolor_list():
-    iris_versicolor = {}
-    count = 1
-    for key in general_list:
-        if 'Iris-versicolor' in general_list[key]:
-            iris_versicolor[count] = general_list[key]
-            count += 1
-    # print(iris_versicolor)
-    count_average(iris_versicolor)
-    return iris_versicolor
-
-
-def iris_virginica_list():
-    iris_virginica = {}
-    count = 1
-    for key in general_list:
-        if 'Iris-virginica' in general_list[key]:
-            iris_virginica[count] = general_list[key]
-            count += 1
-    # print(iris_virginica)
-    count_average(iris_virginica)
-    return iris_virginica
-
-
-def count_average(flower):
-    total = 0
-    for element in flower:
-        total += float(flower[element][0])
-    # print(round(sum, 2))
-    average_growth = total/len(flower)
-    # print(round(average_growth, 2))
-    print(flower[1][4], round(average_growth, 2))
+def count_average(flower):             # used mean built-in function
+    avg = statistics.mean((map(float, (flower[element][0] for element in flower))))
+    print(flower[1][4], round(avg, 2))
 
 
 if __name__ == '__main__':
-    kind_of_flowers()
+    flowers_quantity(general_dictionary())
